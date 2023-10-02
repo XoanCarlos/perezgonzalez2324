e98a9664d80d72cc30b9ad419a98fde46032d10a
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-10-03 00:44:34 +0200

    probando cosas

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import eventos
 from MainWindow import *
 import sys, var
-
 class Main(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_mainWindow()
-        var.ui.setupUi(self) #encargado la interfaz
+        var.ui.setupUi(self) #encargado construir la interfaz
 
         '''
         
@@ -18,7 +17,7 @@ class Main(QtWidgets.QMainWindow):
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = Main()
-    window.show()
+    window.showMaximized()
     sys.exit(app.exec())
 
 
