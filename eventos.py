a09d96452e2d3b146d6c1bd3cf3a088076c2d702
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-10-03 18:48:56 +0200

    calnedar

--- eventos.py
+++ eventos.py
@@ -1,8 +1,21 @@
 import var
+from MainWindow import *
+from CalendarWindow import *
+
 
 class Eventos():
+
     def salir(self):
         try:
             pass
+
         except Exception as error:
             print(error, "en módulo eventos ")
+
+    def abrirCalendar(self):
+
+        try:
+            var.calendar.show()
+        except Exception as error:
+            print('error en abrir calendar ', error)
+
