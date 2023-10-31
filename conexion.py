67415bb87f24a6f5168a01aeca2cea18b1269aa4
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2023-10-31 17:41:18 +0100

    comprobar que los campos están vacíos

--- conexion.py
+++ conexion.py
@@ -49,33 +49,50 @@ class Conexion():
     @staticmethod
     def guardardri(newdriver):
         try:
-            query = QtSql.QSqlQuery()
-            query.prepare('insert into drivers (dnidri, altadri, apeldri, nombredri, direcciondri, provdri, '
-                          ' munidri, movildri, salario, carnet) VALUES (:dni, :alta, :apel, :nombre,:direccion, '
-                          ' :provincia, :municipio, :movil, :salario, :carnet)')
-            query.bindValue(':dni', str(newdriver[0]))
-            query.bindValue(':alta', str(newdriver[1]))
-            query.bindValue(':apel', str(newdriver[2]))
-            query.bindValue(':nombre', str(newdriver[3]))
-            query.bindValue(':direccion', str(newdriver[4]))
-            query.bindValue(':provincia', str(newdriver[5]))
-            query.bindValue(':municipio', str(newdriver[6]))
-            query.bindValue(':movil', str(newdriver[7]))
-            query.bindValue(':salario', str(newdriver[8]))
-            query.bindValue(':carnet', str(newdriver[9]))
-            if query.exec():
-                mbox = QtWidgets.QMessageBox()
-                mbox.setWindowTitle('Aviso')
-                mbox.setIcon(QtWidgets.QMessageBox.Icon.Information)
-                mbox.setText("Empleado dado de alta")
-                mbox.exec()
-            else:
+
+            if (newdriver[0].strip() == "" or newdriver[1].strip() == "" or newdriver[2].strip() == "" or newdriver[3].strip() == "" or newdriver[7].strip() == ""):
                 mbox = QtWidgets.QMessageBox()
                 mbox.setWindowTitle('Aviso')
                 mbox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-                mbox.setText(query.lastError().text())
+                mensaje = 'Faltan Datos. Debe introducir al menos:\n\nDNI, Apellidos, Nombre, Fecha de alta y Móvil'
+                mbox.setText(mensaje)
                 mbox.exec()
-            #select de los datos de conductores de la base de datos
-            #drivers.Drivers.cargartabla(datosdri)
+            else:
+                query = QtSql.QSqlQuery()
+                query.prepare('insert into drivers (dnidri, altadri, apeldri, nombredri, direcciondri, provdri, '
+                              ' munidri, movildri, salario, carnet) VALUES (:dni, :alta, :apel, :nombre,:direccion, '
+                              ' :provincia, :municipio, :movil, :salario, :carnet)')
+                query.bindValue(':dni', str(newdriver[0]))
+                query.bindValue(':alta', str(newdriver[1]))
+                query.bindValue(':apel', str(newdriver[2]))
+                query.bindValue(':nombre', str(newdriver[3]))
+                query.bindValue(':direccion', str(newdriver[4]))
+                query.bindValue(':provincia', str(newdriver[5]))
+                query.bindValue(':municipio', str(newdriver[6]))
+                query.bindValue(':movil', str(newdriver[7]))
+                query.bindValue(':salario', str(newdriver[8]))
+                query.bindValue(':carnet', str(newdriver[9]))
+                if query.exec():
+                    mbox = QtWidgets.QMessageBox()
+                    mbox.setWindowTitle('Aviso')
+                    mbox.setIcon(QtWidgets.QMessageBox.Icon.Information)
+                    mbox.setText("Empleado dado de alta")
+                    mbox.exec()
+                else:
+                    mbox = QtWidgets.QMessageBox()
+                    mbox.setWindowTitle('Aviso')
+                    mbox.setIcon(QtWidgets.QMessageBox.Icon.Warning)
+                    mbox.setText(query.lastError().text())
+                    mbox.exec()
+            registros = []
+            query1 = QtSql.QSqlQuery()
+            query1.prepare("select codigo, apeldri, nombredri, movildri, "
+                           " carnet, bajadri from drivers")
+            if query1.exec():
+                while query1.next():
+                    row = [query1.value(i) for i in range(query1.record().count())]
+                    registros.append(row)
+            print(registros)
+            #drivers.Drivers.cargartabla(registros)
         except Exception as error:
             print("error en alta conductor ", error)
