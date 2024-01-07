382e34172ff8f3db39bb1f54a4dd54e067db79ac
XoanCarlos <jcarlosperezgonzalez@gmail.com>
2024-01-07 14:38:39 +0100

    falta cerrar ventana fecha y modificar alta driver a como está en clientes

--- informes.py
+++ informes.py
@@ -1,4 +1,5 @@
 import os, var, shutil
+from PIL import Image
 from PyQt6 import QtSql, QtWidgets
 from reportlab.pdfgen import canvas
 from datetime import datetime
@@ -21,25 +22,34 @@ class Informes:
             rootPath = '.\\informes'
             for file in os.listdir(rootPath):
                 if file.endswith('listadoclientes.pdf'):
-                    os.startfile('%s\%s' % (rootPath,file))
+                    os.startfile('%s\\%s' % (rootPath, file))
         except Exception as error:
             print('Error LISTADO CLIENTES :', error)
 
     def topInforme(titulo):
         try:
-            logo = svg2rlg('.\img\logo.ico')
-            var.report.line(50, 800, 525, 800)
-            var.report.setFont('Helvetica-Bold', size=14)
-            var.report.drawString(55, 785, 'Transportes Teis')
-            var.report.drawString(240, 695, titulo)
-            var.report.line(50, 690, 525, 690)
-            var.report.drawImage(logo, 480, 725, width=40, height=40)
-            var.report.setFont('Helvetica', size=9)
-            var.report.drawString(55, 770, 'CIF: A12345678')
-            var.report.drawString(55, 755, 'Avda. Galicia - 101')
-            var.report.drawString(55, 740, 'Vigo - 36216 - España')
-            var.report.drawString(55, 725, 'Teléfono: 986 132 456')
-            var.report.drawString(55, 710, 'e-mail: cartesteisr@mail.com')
+            ruta_logo = '.\\img\\logo.ico'
+            logo = Image.open(ruta_logo)
+
+            # Asegúrate de que el objeto 'logo' sea de tipo 'PngImageFile'
+            if isinstance(logo, Image.Image):
+                var.report.line(50, 800, 525, 800)
+                var.report.setFont('Helvetica-Bold', size=14)
+                var.report.drawString(55, 785, 'Transportes Teis')
+                var.report.drawString(240, 695, titulo)
+                var.report.line(50, 690, 525, 690)
+
+                # Dibuja la imagen en el informe
+                var.report.drawImage(ruta_logo, 480, 725, width=40, height=40)
+
+                var.report.setFont('Helvetica', size=9)
+                var.report.drawString(55, 770, 'CIF: A12345678')
+                var.report.drawString(55, 755, 'Avda. Galicia - 101')
+                var.report.drawString(55, 740, 'Vigo - 36216 - España')
+                var.report.drawString(55, 725, 'Teléfono: 986 132 456')
+                var.report.drawString(55, 710, 'e-mail: cartesteisr@mail.com')
+            else:
+                print(f'Error: No se pudo cargar la imagen en {ruta_logo}')
         except Exception as error:
             print('Error en cabecera informe:', error)
 
